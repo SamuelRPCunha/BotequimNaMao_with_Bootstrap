f20767be47e097a13690976d9d6a85bce70990d9
SamuelRPCunha <samuel.rpcunha@gmail.com>
2026-06-01 21:24:50 -0300

    Deploy do Pages

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    // Substitua 'nome-do-repositorio' pelo nome exato do seu projeto no GitHub
+    base: '/BotequimNaMao_with_Bootstrap/',
+});
