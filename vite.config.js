56e9a6cf2469f47276e5fafa9f8e3f46dfd9ca33
SamuelRPCunha <samuel.rpcunha@gmail.com>
2026-06-01 22:30:12 -0300

    Correção erro 404

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,19 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
-    // Substitua 'nome-do-repositorio' pelo nome exato do seu projeto no GitHub
     base: '/BotequimNaMao_with_Bootstrap/',
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                cardapio: resolve(__dirname, 'cardapio.html'),
+                sobre: resolve(__dirname, 'sobre.html'),
+                produto: resolve(__dirname, 'produto.html'),
+                carrinho: resolve(__dirname, 'carrinho.html'),
+                login: resolve(__dirname, 'login.html'),
+                gestao: resolve(__dirname, 'gestao.html')
+            }
+        }
+    }
 });
